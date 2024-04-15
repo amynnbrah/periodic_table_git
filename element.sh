7eb83d7170535174ddf61569e3419a40996498f8
benito fucci <kimou998@gmail.com>
2024-04-15 21:32:49 +0000

    test: trying to add input.

--- element.sh
+++ element.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+if [[ -z $1 ]] 
+then
 echo "Please provide an element as an argument."
+else
+echo "hello"
+fi
