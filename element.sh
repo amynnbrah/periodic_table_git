fea9e54f70e4c8242519cc4d8aca89ba474f8ff0
benito fucci <kimou998@gmail.com>
2024-04-15 23:12:08 +0000

    feat: test is almost done! bash code is working but ugly.

--- element.sh
+++ element.sh
@@ -1,26 +1,54 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 NUMBER='^[0-9]+$'
 
-GET_ELEMENT () {
-  if [[ $1 =~ $NUMBER ]]
+
+GET_ELEMENT_BY_SYMBOL_ORNAME () {
+    ATOM_SYMBOL=$($PSQL "select symbol from elements where symbol ilike '$QUERY'")
+    if [[ -z $ATOM_SYMBOL ]]
+    then
+    ATOM_NAME=$($PSQL "select name from elements where name ilike '$QUERY'")
+    if [[ -z $ATOM_NAME ]]
+    then
+    echo "I could not find that element in the database."
+    else
+  WHOLE_ELEMENT=$($PSQL "select * from elements full join properties using(atomic_number) full join types using(type_id) where name ilike '$QUERY'") 
+  echo "$WHOLE_ELEMENT" | while IFS="|" read TYPEID ATOMICN SYMBOL NAME ATOMICM MELT BOIL TYPE
+  do
+  echo "The element with atomic number $ATOMICN is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMICM amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+  done
+  fi
+fi
+if [[ -n $ATOM_SYMBOL ]]
+then
+ WHOLE_ELEMENT=$($PSQL "select * from elements full join properties using(atomic_number) full join types using(type_id) where symbol ilike '$QUERY'") 
+  echo "$WHOLE_ELEMENT" | while IFS="|" read TYPEID ATOMICN SYMBOL NAME ATOMICM MELT BOIL TYPE
+  do
+  echo "The element with atomic number $ATOMICN is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMICM amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+  done
+fi
+
+}
+
+
+
+GET_ELEMENT_BY_NUMBER () {
+ if [[ $QUERY =~ $NUMBER ]]
   then
-  QUERY=$1
-  ATOMIC_NUMBER=$($PSQL "select name from elements where atomic_number = $QUERY")
+  ATOMIC_NUMBER=$($PSQL "select atomic_number from elements where atomic_number = $QUERY")
   if [[ -z $ATOMIC_NUMBER ]]
   then
   echo "I could not find that element in the database."
   else
-  echo "kakaka"
   #get element using actual atomic nuber
-  WHOLE_ELEMENT=$($PSQL "select * from elements full join properties using(atomic_number) full join types using(type_id) where atomic_number = $ATOMIC_NUMBER") 
-  echo $WHOLE_ELEMENT | while read TYPEID BAR ATOMICN BAR SYMBOL BAR NAME BAR ATOMICM BAR MELT BAR BOIL BAR TYPE
+  WHOLE_ELEMENT=$($PSQL "select * from elements full join properties using(atomic_number) full join types using(type_id) where atomic_number = $QUERY") 
+  echo "$WHOLE_ELEMENT" | while IFS="|" read TYPEID ATOMICN SYMBOL NAME ATOMICM MELT BOIL TYPE
   do
-  echo "The element with atomic number $ATOMICN is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMICM amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
-  echo "HIIIII"
+  echo "The element with atomic number $QUERY is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMICM amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
   done
   fi
-  
+  else
+  GET_ELEMENT_BY_SYMBOL_ORNAME
   fi
 }
 
@@ -29,8 +57,10 @@ if [[ -z $1 ]]
 then
 echo "Please provide an element as an argument."
 else
-GET_ELEMENT
+QUERY=$1
+GET_ELEMENT_BY_NUMBER
 fi
+
 
 
 
